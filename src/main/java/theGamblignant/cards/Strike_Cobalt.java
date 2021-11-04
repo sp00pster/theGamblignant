1ae8c2b4b2adef9513f04e91056543fe28410581
spoopster <minecraftandcake147@gmail.com>
2021-11-04 19:18:20 -0400

    Initial commit

---
+++ src/main/java/theGamblignant/cards/Strike_Cobalt.java
@@ -0,0 +1,70 @@
+package theGamblignant.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theGamblignant.VriskaMod;
+import theGamblignant.characters.TheGamblignant;
+
+import static theGamblignant.VriskaMod.makeCardPath;
+
+public class Strike_Cobalt extends AbstractVriskaCard {
+
+    // TEXT DECLARATION
+
+    public static final String ID = VriskaMod.makeID(Strike_Cobalt.class.getSimpleName());
+    public static final String IMG = makeCardPath("Attack.png");
+    // This does mean that you will need to have an image with the same NAME as the card in your image folder for it to run correctly.
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.BASIC; //  Up to you, I like auto-complete on these
+    private static final CardTarget TARGET = CardTarget.ENEMY;  //   since they don't change much.
+    private static final CardType TYPE = CardType.ATTACK;       //
+    public static final CardColor COLOR = TheGamblignant.Enums.COLOR_COBALT;
+
+    private static final int COST = 1;
+
+    private static final int DAMAGE = 6;
+
+    // /STAT DECLARATION/
+
+
+    public Strike_Cobalt() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = DAMAGE;
+    }
+
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (this.upgraded) {
+            AbstractDungeon.actionManager.addToBottom(
+                    new DamageAction(m, new DamageInfo(p, (damage + roll(6)), damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+        } else {
+            AbstractDungeon.actionManager.addToBottom(
+                    new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+        }
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            this.rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}
