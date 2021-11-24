a5189ccc4e87104bb1f2af12f08f3c9626f93809
spoopster <minecraftandcake147@gmail.com>
2021-11-24 11:59:33 -0500

    added pilfer

---
+++ src/main/java/theGamblignant/actions/PilferAction.java
@@ -0,0 +1,59 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package theGamblignant.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.AbstractGameAction.ActionType;
+import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.utility.WaitAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.vfx.GainPennyEffect;
+import com.megacrit.cardcrawl.vfx.WallopEffect;
+import com.megacrit.cardcrawl.vfx.combat.FlashAtkImgEffect;
+
+public class PilferAction extends AbstractGameAction {
+    private DamageInfo info;
+
+    public PilferAction(AbstractCreature target, DamageInfo info) {
+        this.info = info;
+        this.setValues(target, info);
+        this.actionType = ActionType.DAMAGE;
+        this.startDuration = Settings.ACTION_DUR_FAST;
+        this.duration = this.startDuration;
+    }
+
+    public void update() {
+        if (this.shouldCancelAction()) {
+            this.isDone = true;
+        } else {
+            this.tickDuration();
+            if (this.isDone) {
+                AbstractDungeon.effectList.add(new FlashAtkImgEffect(this.target.hb.cX, this.target.hb.cY, AttackEffect.BLUNT_HEAVY, false));
+                this.target.damage(this.info);
+                if (this.target.lastDamageTaken > 0) {
+                    AbstractDungeon.player.gainGold(this.target.lastDamageTaken);
+
+                    for(int i = 0; i < this.target.lastDamageTaken; ++i) {
+                        AbstractDungeon.effectList.add(new GainPennyEffect(this.source, this.target.hb.cX, this.target.hb.cY, this.source.hb.cX, this.source.hb.cY, true));
+
+                    }
+                }
+
+                if (AbstractDungeon.getCurrRoom().monsters.areMonstersBasicallyDead()) {
+                    AbstractDungeon.actionManager.clearPostCombatActions();
+                } else {
+                    this.addToTop(new WaitAction(0.1F));
+                }
+            }
+
+        }
+    }
+}
--- src/main/java/theGamblignant/cards/Pilfer.java
+++ src/main/java/theGamblignant/cards/Pilfer.java
@@ -2,66 +2,55 @@ package theGamblignant.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.StrengthPower;
 import com.megacrit.cardcrawl.powers.watcher.VigorPower;
 import theGamblignant.VriskaMod;
+import theGamblignant.actions.PilferAction;
 import theGamblignant.characters.TheGamblignant;
 
 import static theGamblignant.VriskaMod.makeCardPath;
 
-public class Strike_Cobalt extends AbstractVriskaCard {
+public class Pilfer extends AbstractVriskaCard {
 
-    // TEXT DECLARATION
-
-    public static final String ID = VriskaMod.makeID(Strike_Cobalt.class.getSimpleName());
-    public static final String IMG = makeCardPath("strike.png");
+    public static final String ID = VriskaMod.makeID(Pilfer.class.getSimpleName());
+    public static final String IMG = makeCardPath("Attack.png");
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
 
-    // /TEXT DECLARATION/
-
-
-    // STAT DECLARATION
-
-    private static final CardRarity RARITY = CardRarity.BASIC; //  Up to you, I like auto-complete on these
-    private static final CardTarget TARGET = CardTarget.ENEMY;  //   since they don't change much.
-    private static final CardType TYPE = CardType.ATTACK;       //
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
     public static final CardColor COLOR = TheGamblignant.Enums.COLOR_COBALT;
 
-    private static final int COST = 1;
+    private static final int COST = 3;
 
-    private static final int DAMAGE = 6;
-
-    // /STAT DECLARATION/
+    private static final int DAMAGE = 8;
 
 
-    public Strike_Cobalt() {
+    public Pilfer() {
         super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
         baseDamage = DAMAGE;
-
-        this.tags.add(CardTags.STARTER_STRIKE);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         if (this.upgraded) {
-            this.baseDamage = roll(6,'a') + 6;
+            this.baseDamage = roll(8,'a') + 8;
             this.calculateCardDamage(m);
         }
-        AbstractDungeon.actionManager.addToBottom(
-                new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+        this.addToBot(new PilferAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn)));
     }
 
     public void applyPowers() {
         super.applyPowers();
         if (this.upgraded) {
-            int addeddamage = 6;
+            int addeddamage = 8;
             if (AbstractDungeon.player.hasPower(StrengthPower.POWER_ID)) {
                 addeddamage += AbstractDungeon.player.getPower(StrengthPower.POWER_ID).amount;
             }
